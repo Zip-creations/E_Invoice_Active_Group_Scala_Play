c2186a7e77a35ea18eb27c46937cd20c2062f735
Zip-creations <ruben.rosenmeyer@gmail.com>
2025-04-19 17:09:39 +0200

    codelists can now be directly converted from the original .xsd

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const testlist = ["0", "test1", "test2", "test3", "test4", "test5", "test6", "test7", "test8", "test9", "test10"];
     var invoiceDateInput = document.getElementsByName("InvoiceIssueDate");
     invoiceDateInput.forEach(function (invoiceDateInput) {
         invoiceDateInput.setAttribute("value", new Date().toISOString().split("T")[0]);
@@ -19,31 +18,32 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     document.querySelectorAll("input.awesomplete").forEach(input => {
-        new Awesomplete(input, {
-          minChars: 1,
-          maxItems: 6,
-          autoFirst: true,
-          tabSelect: true,
-          // TODO: Give every awesomplete a unique list, corresponding to the requirements in the XRechnung
-          list: testlist,
-          // filter: Awesomplete.FILTER_STARTSWITH,
-        });
-        console.log("here")
+        ConnectData(input.dataset.file).then(values => {
+            // Set the data the completionchecker uses
+            input.data = values
+            // Set awesomplete properties
+            new Awesomplete(input, {
+              minChars: 0,
+              maxItems: 20,
+              autoFirst: true,
+              tabSelect: true,
+              list: values,
+              filter: Awesomplete.FILTER_STARTSWITH,
+            });
+        })
     });
 
     document.querySelectorAll("input.completionchecker").forEach(input => {
         input.addEventListener("beforeinput", (e) => {
             const { selectionStart, selectionEnd, value } = input;
-            // Create input the user wants to type in. Slicing is necessary to test in-between inserts,
+            // Create the input the user wants to type in. Slicing is necessary to test in-between inserts,
             // and in case several letters are marked at once with the cursor
-            const proposed =
-                value.slice(0, selectionStart) +
-                e.data +
-                value.slice(selectionEnd);
-            // TODO: check the individual list of the input, instead of testlist
-            const matches = testlist.some(entry => entry.startsWith(proposed));
-            // e.data is null if DELETE or BACKSPACE are pressed (which needs to be allowed)
-            if (!matches && !(e.data === null)) {
+            const proposed = value.slice(0, selectionStart) + e.data + value.slice(selectionEnd);
+            const matches = input.data.some(entry => entry.startsWith(proposed));
+            // e.data is null if DELETE or BACKSPACE are pressed
+            // (which must be allowed, but only from the end of the text to prevent false inputs)
+            // BACKSPACE is non-functional due to this restriction
+            if (!matches && !(e.data === null && selectionEnd === value.length)) {
                 e.preventDefault();
             }
         });
@@ -51,6 +51,12 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
+/**
+ *Fetches invoiceLine.scala.html, tranfers the current PostionID, attaches the created inputContainer
+ * (which contains one invoice position) and increments the positionID
+ * @param {*} positionID consistent, increasing ID for invoice Positions
+ * @returns 
+ */
 function CreatePosition(positionID) {
     fetch(`/invoiceLine?positionID=${positionID.toString()}`)
         .then(response => response.text())
@@ -58,7 +64,25 @@ function CreatePosition(positionID) {
             const targetDiv = document.getElementById("positionContainer");
             targetDiv.insertAdjacentHTML("beforeend", html);
         })
-    .catch(error => console.error("Fehler beim Laden der Position:", error));
     positionID++
     return positionID
+}
+
+
+/**
+ *  Reads values from a .xsd file and returns them as Array. Assumes the format: <xsd:enumeration value="foo"/>
+ *  @param {*} positionID filename for .xsd
+ */
+// Needs to be async because values is set in the fetch .then part
+async function ConnectData(filename) {
+    var values = []
+    // Will be translated to public/codelists/filename
+    filepath = "assets/codelists/" + filename
+    await fetch(filepath)
+        .then(response => response.text())
+        .then(xsdtext => {
+            const matches = Array.from(xsdtext.matchAll(/<xsd:enumeration value="(.*?)"/g));
+            values = matches.map(match => match[1]);
+        })
+    return values
 }
